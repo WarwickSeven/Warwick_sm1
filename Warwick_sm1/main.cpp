c2c45c80f970892c3c2abb6ff1eb65c2a9ee8a30
WarwickSeven <WarwickSeven@gmail.com>
2022-05-18 13:55:56 +0300

    clear main

--- Warwick_sm1/main.cpp
+++ Warwick_sm1/main.cpp
@@ -1,14 +1,1 @@
-//
-//  main.cpp
-//  Warwick_sm1
-//
-//  Created by Александр Тихонов on 18.05.2022.
-//
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
